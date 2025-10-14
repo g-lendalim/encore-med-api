7da762505f0469c0dfdfd96f2ebe7df20291b768
Glenda Lim <freyjaglenda@gmail.com>
2025-10-15 01:11:19 +0800

    Export enum sexEnum

--- src/hospital/dto/create-hospital.dto.ts
+++ src/hospital/dto/create-hospital.dto.ts
@@ -9,6 +9,11 @@ import {
 import { Type } from 'class-transformer';
 import { Sex } from '@prisma/client';
 
+export enum SexEnum {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+}
+
 class AdminInfo {
   @IsEmail()
   email: string;
